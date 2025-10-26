9f4ac025c4c4010fb042b874102918ed9b9d9ac6
Naman Singla <namansingla830@gmail.com>
2025-10-26 21:37:00 +0530

    use loader to get fast in github

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,22 +1,28 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const Github = () => {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        fetch('https://api.github.com/users/hiteshchoudhary')
-        .then(response => response.json() )
-        .then(data => {
-            console.log(data);
-            setData(data)
-        })
-    }, []);
-    return (
-        <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl flex justify-center items-center flex-col gap-6'>
-            Github Followers: {data.followers}
-            <img className="" src={data.avatar_url} alt="Git piture" width={300}/>
-        </div>  
-    );
-}
+    const data = useLoaderData()
+//   const [data, setData] = useState([]);
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/hiteshchoudhary")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex justify-center items-center flex-col gap-6">
+      Github Followers: {data.followers}
+      <img className="" src={data.avatar_url} alt="Git piture" width={300} />
+    </div>
+  );
+};
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/hiteshchoudhary");
+  return response.json();
+};
